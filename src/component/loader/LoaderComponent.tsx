afa70844a867c597d9ab73a7cf3d6d9b00f3ae20
mik284 <mikeykamau222@gmail.com>
2024-08-15 10:59:19 +0300

    feat: add loader component

---
+++ src/component/loader/LoaderComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './loading.css'
+
+interface Props {}
+
+function LoaderComponent(props: Props) {
+    const {} = props
+
+    return (
+        <div className="loading"></div>
+    )
+}
+
+export default LoaderComponent
